aa11db10153afedc57f9e1d6bcdee43e86f9b44e
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-11-24 23:50:01 -0800

    helper functions for return start addr's

---
+++ inc/helper.h
@@ -0,0 +1,2 @@
+unsigned int checkFileType(unsigned char *bytes_read);
+struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim);
---
+++ src/helper.c
@@ -0,0 +1,43 @@
+#include "../inc/disass.h"
+#include "../inc/helper.h"
+
+unsigned int checkFileType(unsigned char *bytes_read)
+{
+  unsigned int header;
+  unsigned int code_address = 0x000;
+
+  header = (bytes_read[0] << 24) | (bytes_read[1] << 16)| (bytes_read[2] << 8 )
+          | bytes_read[3];
+
+  if(header == 0x7f454c46)
+  {
+    printf("[+] ELF Magic Header Found: 0x%x\n", header);
+    code_address += 0x10F;
+  }
+  else if(header == 0x4d5a9000) {
+    printf("[+] PE Magic Header Found: 0x%x\n", header);
+    code_address += 0x10F;
+  }
+  else if(header == 0xcffaedfe) {
+    printf("[+] Mach-O Magic Header Found: 0x%x\n", header);
+    code_address += 0x10F;
+  }
+  else {
+    printf("[!] Could not find a proper header, results may vary\n");
+  }
+  printf("========== Blade Disassembler ==========\n");
+
+  return code_address;
+}
+
+// store multiple values
+struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim)
+{
+  struct storeSplitStr storeStr;
+  int i = 0;
+  char *copy = strdup(strToSplit);
+  storeStr.reg1 = strtok(copy, delim);
+  storeStr.reg2 = strtok(NULL, delim);
+
+  return storeStr;
+}
