b2c176de43dc2ce4ce51bdd3a5a761f465758721
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-11-24 20:57:55 -0800

    change opcode assumption for xchg

---
+++ src/disass.c
@@ -0,0 +1,155 @@
+#include "../inc/disass.h"
+
+// helper functions
+
+// store multiple values
+struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim)
+{
+  struct storeSplitStr storeStr;
+  int i = 0;
+  char *copy = strdup(strToSplit);
+  storeStr.reg1 = strtok(copy, delim);
+  storeStr.reg2 = strtok(NULL, delim);
+
+  return storeStr;
+}
+
+
+// dissass functions
+
+// each enum value returns a number from instruction
+int ReturnInstructionNumber(unsigned char* opcode, int value)
+{
+  struct instrReg instreg;
+
+  if(opcode[value] >= 0x50 && opcode[value] <= 0x68)
+  {
+    // 68 opcode holds a special value for PUSH
+    instreg.instruction = PUSH;
+    return instreg.instruction;
+  }
+  else if(opcode[value] == 0xcd) {
+    // syscalls
+    instreg.instruction = INT;
+    return instreg.instruction;
+  }
+  else if(opcode[value] == 0x30 || opcode[value] == 0x31) {
+    instreg.instruction = XOR;
+    return instreg.instruction;
+  }
+  else if(opcode[value] == 0xf6 || opcode[value] == 0xf7) {
+    instreg.instruction = MUL;
+    return instreg.instruction;
+  }
+  else if(opcode[value] == 0xb0 || opcode[value] == 0x89) {
+    instreg.instruction = MOV;
+    return instreg.instruction;
+  }
+  else if(opcode[value] == 0x87 || opcode[value] == 0x93) {
+    instreg.instruction = XCHG;
+    return instreg.instruction;
+  }
+  else {
+    // opcode 90
+    instreg.instruction = NOP;
+    return instreg.instruction;
+  }
+}
+
+// each enum value returns a number from register
+int ReturnRegisterNumber(unsigned char* opcode, int value)
+{
+  struct instrReg instreg;
+
+  if(opcode[value] == 0x50) {
+    instreg.registr = EAX;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0x51) {
+    instreg.registr = ECX;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0x93) {
+    instreg.registr = EBXEAX;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0x68 || opcode[value] == 0xcd) {
+    instreg.registr = SPEC;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0x31 && opcode[value+1] == 0xc9) {
+    instreg.registr = ECX;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0xf7 && opcode[value+1] == 0xe1) {
+    instreg.registr = ECX;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0xb0 && opcode[value+1] >= 0x00) {
+    instreg.registr = AL;
+    return instreg.registr;
+  }
+  else if(opcode[value] == 0x89 && opcode[value+1] == 0xe3) {
+    instreg.registr = EBXESP;
+    return instreg.registr;
+  }
+  else {
+    instreg.registr = EIP;
+    return instreg.registr;
+  }
+}
+
+void printAssemblyCode(const char* instr, const char* reg, unsigned char* bytes_read, int i)
+{
+  struct storeSplitStr storedStr;
+
+  // Skip anything that returns null
+  if(strncmp("EIP", reg, strlen(reg)) != 0)
+  {
+    if(strncmp("MOV", instr, strlen(instr)) == 0)
+    {
+      if(bytes_read[i] == 0x89)
+      {
+        // once we hit an 0x89 opcode, we have series range of what gets mov'd
+        if(bytes_read[i+1] == 0xe3)
+        {
+          storedStr = splitStrStart(reg, " ");
+          printf("%s      %s, %s\n", instr, storedStr.reg1, storedStr.reg2);
+        }
+      }
+      // just mov'ing a number into a register
+      else {
+        printf("%s      %s, 0x%x\n", instr, reg, (int)bytes_read[i+1]);
+      }
+      i += 1;
+    }
+    else if(strncmp("XCHG", instr, strlen(instr)) == 0) {
+      // series of opcodes for xchg we will have to move in
+      if(bytes_read[i] == 0x93)
+      {
+        storedStr = splitStrStart(reg, " ");
+        printf("%s     %s, %s\n", instr, storedStr.reg1, storedStr.reg2);
+      }
+    }
+    else if(strncmp("XOR", instr, strlen(instr)) == 0) {
+      printf("%s      %s, %s\n", instr, reg, reg);
+    }
+    else if(strncmp("SPEC", reg, strlen(reg)) == 0) {
+      if(strncmp("PUSH", instr, strlen(reg)) == 0)
+      {
+        printf("%s     0x%x%x%x%x\n", instr, (int)bytes_read[i+4], (int)bytes_read[i+3],
+            (int)bytes_read[i+2], (int)bytes_read[i+1]);
+        // jump further into the loop, i jumps four bytes
+        i += 4;
+      }
+      else if(strncmp("INT", instr, strlen(reg)) == 0) {
+        printf("%s      0x%x\n", instr, (int)bytes_read[i+1]);
+        // jump further into the loop, i jumps four bytes
+        i += 1;
+      }
+    }
+    else {
+      printf("%s      %s\n", instr, reg);
+    }
+  }
+}
