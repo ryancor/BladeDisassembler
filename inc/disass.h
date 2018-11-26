4d4413495e2df5cf38acb58e22dfb362e43aa3a6
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-11-26 13:38:59 -0800

    moved usage over and std bool

--- inc/disass.h
+++ inc/disass.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 int ReturnInstructionNumber(unsigned char* opcode, int value);
--- inc/helper.h
+++ inc/helper.h
@@ -1,2 +1,5 @@
+#include <stdbool.h>
+
+bool usage(int argc, char* argv, char* argve);
 unsigned int checkFileType(unsigned char *bytes_read);
 struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim);
