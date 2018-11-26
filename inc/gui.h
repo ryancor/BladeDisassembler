88b1ccc5c2a74e23679492654a252341955cf6ba
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-11-26 13:19:37 -0800

    unix friendly gui to display disass code

---
+++ inc/gui.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+
+#define filename      "sampleoslr.blade"
+
+void show_gui_init(int argc, char *argv[]);
---
+++ src/gui.c
@@ -0,0 +1,120 @@
+#include "../inc/gui.h"
+
+void check_file_size(FILE *file1)
+{
+  int size;
+
+  if(file1 == NULL)
+  {
+    printf("File does not exist\n");
+    exit(-1);
+  }
+
+  fseek(file1, 0, SEEK_END);
+  size = ftell(file1);
+  fseek(file1, 0, SEEK_SET);
+
+  if(size >= 8000)
+  {
+    printf("File size too big : %d\n", size);
+    exit(-1);
+  }
+}
+
+void modeselect(GtkWidget *widget, gpointer view)
+{
+  char file_array2[8192];
+  FILE *file2;
+  char word;
+  char file_array1[8192];
+  gchar *gbuf;
+  FILE *file1;
+
+  GtkTextBuffer *buffer;
+
+  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+
+  gchar *text = gtk_combo_box_get_active_text(GTK_COMBO_BOX(widget));
+
+  if(strcmp(text, "OLSR MODE") == 0)
+  {
+    file1 = fopen(filename, "r");
+    check_file_size(file1);
+
+    while((word = fgetc(file1)) != EOF)
+    {
+      strcat(file_array1, &word);
+    }
+    gbuf = g_strdup_printf("%s", (gchar *)file_array1);
+
+    fclose(file1);
+
+    gtk_text_buffer_set_text(buffer, gbuf, -1);
+  }
+}
+
+void show_gui_init(int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *view;
+  GtkWidget *frame;
+  GtkWidget *frame2;
+  GtkWidget *vbox;
+  GtkWidget *vbox2;
+  GtkWidget *hbox;
+  GtkWidget *label;
+  GtkWidget *label2;
+  GtkWidget *combo;
+
+  GtkTextBuffer *buffer;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  gtk_window_set_default_size(GTK_WINDOW(window), 650, 600);
+  gtk_window_set_title(GTK_WINDOW(window), "Blade Disassembler");
+  gtk_container_set_border_width(GTK_CONTAINER(window), 5);
+  GTK_WINDOW(window)->allow_shrink = TRUE;
+
+  frame = gtk_fixed_new();
+  frame2 = gtk_fixed_new();
+  vbox = gtk_vbox_new(FALSE, 0);
+  vbox2 = gtk_vbox_new(FALSE, 0);
+  hbox = gtk_hbox_new(FALSE, 0);
+
+  combo = gtk_combo_box_new_text();
+  gtk_combo_box_append_text(GTK_COMBO_BOX(combo), "--Pick One--");
+  gtk_combo_box_append_text(GTK_COMBO_BOX(combo), "OLSR MODE");
+
+  view = gtk_text_view_new();
+  gtk_widget_set_size_request(view, 0, 550);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW (view), FALSE);
+  gtk_box_pack_start(GTK_BOX(vbox2), view, TRUE, TRUE, 0);
+  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+
+  label = gtk_label_new( NULL);
+  gtk_widget_set_size_request(label, 650, 500);
+
+  label2 = gtk_label_new("Select the mode");
+
+  gtk_fixed_put(GTK_FIXED(frame), label, 10, 10);
+
+  gtk_fixed_put(GTK_FIXED(frame2), label2, 20, 25);
+  gtk_fixed_put(GTK_FIXED(frame2), combo, 200, 20);
+
+  gtk_box_pack_start(GTK_BOX(hbox), frame2, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), vbox2, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), hbox, TRUE, TRUE, 0);
+
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  g_signal_connect_swapped(G_OBJECT(window), "destroy",
+                           G_CALLBACK(gtk_main_quit), G_OBJECT(window));
+
+  g_signal_connect(G_OBJECT (combo),"changed", G_CALLBACK(modeselect),(gpointer) view);
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+}
