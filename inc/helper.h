3e3b0f30a5918067c0f5e58867d5271d02b8cffe
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-12-03 11:01:34 -0800

    macros for PE and ELF function hops

--- inc/helper.h
+++ inc/helper.h
@@ -1,12 +1,22 @@
 #include <stdbool.h>
+#include <time.h>
 
 #define ELF_HEADER        0x7f454c46
 #define PE_HEADER         0x4d5a9000
 #define MACHO_HEADER      0xcffaedfe
 
-#define ELF_EOF           0x74657874
+#define ELF_EOFILE        0x74657874
+#define ELF_EOFUNC1       0xE910FFFF
+#define ELF_EOFUNC2       0xC3669066
+#define ELF_EOFUNC3       0x66906690
+#define ELF_EOFUNC4       0x90669090
+
+#define PE_EOFUNC1        0xC38D7600
+#define PE_EOFUNC2        0x8B442404
+#define PE_EOFUNC3        0x8D762600
 
 bool usage(int argc, char* argv, char* argve);
 unsigned int checkFileType(unsigned char *bytes_read);
 bool checkEndOfFile(unsigned char *bytes_read, int value);
+void labelFunctions(unsigned char *bytes_read, int value, unsigned int startAddr);
 struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim);
