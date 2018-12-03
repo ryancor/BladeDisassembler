9bf765ba9f592f43e2d6a6143b270997c11b87f9
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-12-03 11:14:27 -0800

    define dll header for end of .rdata

--- inc/helper.h
+++ inc/helper.h
@@ -11,6 +11,8 @@
 #define ELF_EOFUNC3       0x66906690
 #define ELF_EOFUNC4       0x90669090
 
+#define PE_EOFILE         0xB86F4100
+#define PE_EOFILE_DLL     0x38610010
 #define PE_EOFUNC1        0xC38D7600
 #define PE_EOFUNC2        0x8B442404
 #define PE_EOFUNC3        0x8D762600
