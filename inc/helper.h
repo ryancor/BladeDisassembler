b5b4282da075521ebe5cccfb04eea6689c583a3b
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-12-02 17:35:27 -0800

    define executable header symbols

--- inc/helper.h
+++ inc/helper.h
@@ -1,5 +1,12 @@
 #include <stdbool.h>
 
+#define ELF_HEADER        0x7f454c46
+#define PE_HEADER         0x4d5a9000
+#define MACHO_HEADER      0xcffaedfe
+
+#define ELF_EOF           0x74657874
+
 bool usage(int argc, char* argv, char* argve);
 unsigned int checkFileType(unsigned char *bytes_read);
+bool checkEndOfFile(unsigned char *bytes_read, int value);
 struct storeSplitStr splitStrStart(const char * strToSplit, const char * delim);
