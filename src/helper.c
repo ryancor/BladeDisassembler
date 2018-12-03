332cac36f35662d9fd5b52f2d39ef5960902defe
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-12-02 17:36:00 -0800

    bool function type for checking end of symbol file

--- src/helper.c
+++ src/helper.c
@@ -24,22 +24,20 @@ bool usage(int argc, char* argv, char* argve)
 
 unsigned int checkFileType(unsigned char *bytes_read)
 {
-  unsigned int header;
+  unsigned int header = (bytes_read[0] << 24) | (bytes_read[1] << 16)
+          | (bytes_read[2] << 8 ) | bytes_read[3];
   unsigned int code_address = 0x000;
 
-  header = (bytes_read[0] << 24) | (bytes_read[1] << 16)| (bytes_read[2] << 8 )
-          | bytes_read[3];
-
-  if(header == 0x7f454c46)
+  if(header == ELF_HEADER)
   {
     printf("[+] ELF Magic Header Found: 0x%x\n", header);
     code_address += 0x110;
   }
-  else if(header == 0x4d5a9000) {
+  else if(header == PE_HEADER) {
     printf("[+] PE Magic Header Found: 0x%x\n", header);
     code_address += 0x110;
   }
-  else if(header == 0xcffaedfe) {
+  else if(header == MACHO_HEADER) {
     printf("[+] Mach-O Magic Header Found: 0x%x\n", header);
     code_address += 0x110;
   }
@@ -49,6 +47,23 @@ unsigned int checkFileType(unsigned char *bytes_read)
   printf("========== Blade Disassembler ==========\n");
 
   return code_address;
+}
+
+bool checkEndOfFile(unsigned char *bytes_read, int value)
+{
+  unsigned int header = (bytes_read[0] << 24) | (bytes_read[1] << 16)
+          | (bytes_read[2] << 8 ) | bytes_read[3];
+  unsigned int symbol = (bytes_read[value] << 24) | (bytes_read[value+1] << 16)
+          | (bytes_read[value+2] << 8) | bytes_read[value+3];
+  bool endFlag = false;
+
+  // find .text section symbol, then check what type of executable
+  if(symbol == ELF_EOF && header == ELF_HEADER)
+  {
+    printf("=== End of ELF file ===\n");
+    endFlag = true;
+  }
+  return endFlag;
 }
 
 // store multiple values
